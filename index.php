0b737c97904277f086b07e53fb7f5bf378877d5c
gazzarico <47733054+gazzarico@users.noreply.github.com>
2019-02-26 15:27:15 +1100

    finished week 2

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Back end web design</title>
+        <meta name="description" content="I can't remember.">
+        <link rel="stylesheet" href="main.css">
+    </head>
+    
+
+    <body>
+        
+
+
+<?php echo "Hello world!"; ?>
+
+    </body>
+    
+</html>
